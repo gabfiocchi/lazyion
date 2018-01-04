574f9909cd33a660112e5a0add86f99d1fe45302
Gabriel Fiocchi <gab@blowapp.com.ar>
2018-01-04 01:22:31 -0300

    init repo.

---
+++ bin/cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict';
+
+const commander = require('commander');
+const packageJson = require('../package.json');
+
+
+commander.command('fullname').alias('f')
+    .description('Print Full Name')
+    .action(() => console.log('hola'));
+
+commander.command('contact').alias('c')
+    .description('Print Contact Info')
+    .action(() => console.log('holaaa'));
+
+commander.parse(process.argv);
